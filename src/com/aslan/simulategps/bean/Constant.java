c1f39c67ac526bda4876c2c44600d994a175bdf3
huweiko <hwboot@163.com>
2015-12-18 23:25:14 +0800

    1玩儿

---
+++ src/com/aslan/simulategps/bean/Constant.java
@@ -0,0 +1,19 @@
+package com.aslan.simulategps.bean;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class Constant {
+
+	public static final long MIN_LOGINTIME = 0;
+	public static class Preference {
+
+		public static final String SERVERIP = "SERVERIP";
+		public static final String PORT = "PORT";
+
+		public static SharedPreferences getSharedPreferences(Context context) {
+			return context.getSharedPreferences("SimulateGpsSharePref", Context.MODE_PRIVATE);
+		}
+	}
+
+}
